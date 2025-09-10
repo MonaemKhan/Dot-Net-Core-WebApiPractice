5527a985d25cfc54c096ed8342cd342720a4cbc3
M.A Monaem Khan <khanmonaem07@gmail.com>
2025-09-10 18:13:32 +0600

    data collection

---
+++ API-VERSIONING-AND-DESIGN-PATTERN/MedXDataCollection/DataCollection.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace MedXDataCollection
+{
+    public class DataCollection
+    {
+        public string? URL { get; set; }
+        public string? TITLE { get; set; }
+        public byte[] IMAGE { get; set; }
+        public string? IMAGE_URL { get; set; }
+        public string? NAME { get; set; }
+        public string? STREANGTH { get; set; }
+        public string? FULLNAME { get; private set; }
+        public void FULLNAMESET()
+        {
+            FULLNAME = $"{NAME} {STREANGTH}";
+        }
+    }
+}
--- API-VERSIONING-AND-DESIGN-PATTERN/MedXDataCollection/Program.cs
+++ API-VERSIONING-AND-DESIGN-PATTERN/MedXDataCollection/Program.cs
@@ -1,31 +1,111 @@
-﻿using System.Text.RegularExpressions;
+﻿using MedXDataCollection;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 
 HttpClient _http = new HttpClient();
 
-var url = $"http://medex.com.bd/ajax/search?searchtype=search&searchkey={"xyzv"}";
-var html = await _http.GetStringAsync(url);
-
-var data = html.Split("</a>");
+HashSet<DataCollection> dataCollections = new HashSet<DataCollection>();
 
 string pattern = @"<a\s+href=""(?<url>[^""]+)"".*?<li\s+title=""(?<title>[^""]+)"".*?<img\s+src=""(?<img>[^""]+)""[^>]*>.*?<span>\s*(?<name>.*?)\s*<span[^>]*>(?<strength>[^<]+)</span>";
 
-
-foreach (var item in data)
+for (char c = 'a'; c <= 'z'; c++)
 {
-    var match = Regex.Match(item, pattern, RegexOptions.Singleline | RegexOptions.IgnoreCase);
-
-    if (match.Success)
+    string searchstr = c + "";
+    for (char d = 'a'; d <= 'z'; d++)
     {
-        string url1 = match.Groups["url"].Value;
-        string title = match.Groups["title"].Value;
-        string img = match.Groups["img"].Value;
-        string name = match.Groups["name"].Value.Trim();
-        string strength = match.Groups["strength"].Value.Trim();
-        string fullName = $"{name} {strength}";
+        string searchstr2 = d + "";
+        if (searchstr != searchstr2)
+        {
+            searchstr2 = searchstr + searchstr2;
+        }
+        //Console.WriteLine(searchstr2);
 
-        Console.WriteLine($"URL: {url1}");
-        Console.WriteLine($"Title: {title}");
-        Console.WriteLine($"Image URL: {img}");
-        Console.WriteLine($"Medicine Name: {fullName}");
+        var url = $"http://medex.com.bd/ajax/search?searchtype=search&searchkey={searchstr2}";
+        
+        var html = await _http.GetStringAsync(url);
+
+        var data = html.Split("</a>");       
+
+
+        foreach (var item in data)
+        {
+            var match = Regex.Match(item, pattern, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+            if (match.Success)
+            {
+                var dataCollection = new DataCollection
+                {
+                    URL = match.Groups["url"].Value,
+                    TITLE = match.Groups["title"].Value,
+                    IMAGE_URL = match.Groups["img"].Value,
+                    NAME = match.Groups["name"].Value.Trim(),
+                    STREANGTH = match.Groups["strength"].Value.Trim(),
+                };
+                dataCollection.FULLNAMESET();
+                dataCollections.Add(dataCollection);
+            }
+        }
+
+        for (char e = 'a'; e <= 'z'; e++)
+        {
+            string searchstr3 = e + "";
+            if (searchstr2 != searchstr3)
+            {
+                searchstr3 = searchstr2 + searchstr3;
+            }
+            if (searchstr3.Length < 3)
+                break;
+            url = ""; 
+            html = ""; 
+            data=null;
+
+            url = $"http://medex.com.bd/ajax/search?searchtype=search&searchkey={searchstr3}";
+
+            html = await _http.GetStringAsync(url);
+
+            data = html.Split("</a>");
+
+
+            foreach (var item in data)
+            {
+                var match = Regex.Match(item, pattern, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+                if (match.Success)
+                {
+                    var dataCollection = new DataCollection
+                    {
+                        URL = match.Groups["url"].Value,
+                        TITLE = match.Groups["title"].Value,
+                        IMAGE_URL = match.Groups["img"].Value,
+                        NAME = match.Groups["name"].Value.Trim(),
+                        STREANGTH = match.Groups["strength"].Value.Trim(),
+                    };
+                    dataCollection.FULLNAMESET();
+                    dataCollections.Add(dataCollection);
+                }
+            }
+
+        }
+        url = "";
+        html = "";
+        data = null;
     }
+    Console.WriteLine(c+" Finished !!!!");
 }
+
+string filePath = @"D:\Github Repos\Dot-Net-Core-WebApiPractice\API-VERSIONING-AND-DESIGN-PATTERN\MedXDataCollection\jsonData\medicineTempList.json";
+
+// JSON options (pretty print)
+var options = new JsonSerializerOptions { WriteIndented = true };
+
+// Serialize list to JSON
+string json = JsonSerializer.Serialize(dataCollections, options);
+
+// Save JSON to file
+File.WriteAllText(filePath, json, Encoding.UTF8);
+
+Console.WriteLine($"JSON file saved at {filePath}");
+
+Console.WriteLine($"Total Medicines: {dataCollections.Count}");
+
