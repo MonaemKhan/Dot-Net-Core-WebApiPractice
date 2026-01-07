2c7ad6e16516988411e8305e167258117d6e2f27
M.A Monaem Khan <khanmonaem07@gmail.com>
2026-01-07 11:15:49 +0600

    ok

---
+++ API-VERSIONING-AND-DESIGN-PATTERN-AND-OTHERS/PUBLICKEY_PRIVATEKEY_PAIR/Controllers/RSAPAIRController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PUBLICKEY_PRIVATEKEY_PAIR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RSAPAIRController : ControllerBase
+    {
+        private readonly RsaKeyService _rsaKeyService = new RsaKeyService();
+        private string _publicKey;
+        private string _privateKey;
+
+        public RSAPAIRController()
+        {
+            var (publicKey, privateKey) = _rsaKeyService.GenerateKeys();
+            _publicKey = publicKey;
+            _privateKey = privateKey;
+        }
+
+        // GET: api/<RSAPAIRController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new {
+                publicKey = _publicKey,
+                privateKey = _privateKey
+            });
+        }
+
+        [HttpGet("encrypt")]
+        public IActionResult Encrypt(string plainText)
+        {
+            var encryptedText = EncryptData(plainText, _publicKey);
+            return Ok(new { encryptedText = Convert.ToBase64String(encryptedText) });
+        }
+
+        [HttpGet("decrypt")]
+        public IActionResult Decrypt(string plainText)
+        {
+            var encryptedText = DecryptData(Convert.FromBase64String(plainText), _privateKey);
+            return Ok(new { encryptedText = encryptedText });
+        }
+
+        private byte[] EncryptData(string plainText, string publicKeyPem)
+        {
+            using var rsa = RSA.Create();
+            rsa.ImportFromPem(publicKeyPem.ToCharArray());
+            return rsa.Encrypt(Encoding.UTF8.GetBytes(plainText), RSAEncryptionPadding.Pkcs1);
+        }
+
+        private string DecryptData(byte[] cipherText, string privateKeyPem)
+        {
+            using var rsa = RSA.Create();
+            rsa.ImportFromPem(privateKeyPem.ToCharArray());
+            var decryptedBytes = rsa.Decrypt(cipherText, RSAEncryptionPadding.Pkcs1);
+            return Encoding.UTF8.GetString(decryptedBytes);
+        }
+
+    }
+}
---
+++ API-VERSIONING-AND-DESIGN-PATTERN-AND-OTHERS/PUBLICKEY_PRIVATEKEY_PAIR/RsaKeyService.cs
@@ -0,0 +1,47 @@
+﻿using MedXDataCollection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+namespace PUBLICKEY_PRIVATEKEY_PAIR
+{
+    public class RsaKeyService
+    {
+        public (string publicKey, string privateKey) GenerateKeys(int keySize = 2048)
+        {
+            using var rsa = RSA.Create(keySize);
+
+            var publicKey = ExportPublicKey(rsa);
+            var privateKey = ExportPrivateKey(rsa);
+
+            return (publicKey, privateKey);
+        }
+
+        private string ExportPublicKey(RSA rsa)
+        {
+            var keyBytes = rsa.ExportSubjectPublicKeyInfo();
+            return ToPem("PUBLIC KEY", keyBytes);
+        }
+
+        private string ExportPrivateKey(RSA rsa)
+        {
+            var keyBytes = rsa.ExportPkcs8PrivateKey();
+            return ToPem("PRIVATE KEY", keyBytes);
+        }
+
+        private string ToPem(string title, byte[] data)
+        {
+            var base64 = Convert.ToBase64String(data);
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"-----BEGIN {title}-----");
+
+            for (int i = 0; i < base64.Length; i += 64)
+                sb.AppendLine(base64.Substring(i, Math.Min(64, base64.Length - i)));
+
+            sb.AppendLine($"-----END {title}-----");
+
+            return sb.ToString();
+        }
+    }
+}
